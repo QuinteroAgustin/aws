9f1e7bb6dc9179b6168686e1b2ee1734702e7b83
Quintero Agustin <agustin.quintero@limayrac.fr>
2022-04-06 11:46:56 +0200

    gigi

--- index.php
+++ index.php
@@ -1,5 +1,27 @@
-<?php
-    $title = "Jules le PD";
-?>
-
-<h1><?=$title?></h1>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Navigation - XAMPP</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <h1>PhpMyAdmin</h1>
+    <p>Lien pour <a href="adminer.php">Adminer</a></p>
+    <h1>Navigation exos</h1>
+    <table>
+        <tr>
+            <th>POO</th>
+            
+        </tr>
+        <tr>
+            <td>
+                <ul>
+                    <li><a href="poo/poo_f/exo.php">POO_F</a></li>
+                </ul>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
